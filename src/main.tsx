0b5ef339a22831e8b9d9f06c8b54238bc101b892
Prajwala <praj.p98@gmail.com>
2026-05-26 17:39:16 +0530

    Wrap App component with AuthProvider, CartProvider, and BrowserRouter in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router";
 import App from "./App";
+import { AuthProvider } from "./context/AuthContext";
+import { CartProvider } from "./context/CartContext";
 import { ToastProvider } from "./components/ui/Toast";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ToastProvider>
-      <App />
-    </ToastProvider>
+    <BrowserRouter>
+      <AuthProvider>
+        <CartProvider>
+          <ToastProvider>
+            <App />
+          </ToastProvider>
+        </CartProvider>
+      </AuthProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
