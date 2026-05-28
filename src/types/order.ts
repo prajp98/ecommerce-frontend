88a4ab0ea8173198b1ad991bd429d57741d2926d
prajwalaparameshwar <praj.p98@gmail.com>
2026-05-28 12:32:31 +0530

    Created new order type

---
+++ src/types/order.ts
@@ -0,0 +1,41 @@
+export type OrderItem = {
+    orderItemId: number;
+    productId: number;
+    productName: string;
+    quantity: number;
+    priceAtPurchase: number;
+    totalPrice: number;
+  };
+  
+  export type Order = {
+    orderId: number;
+    orderNumber: string;
+    status: string;
+    totalAmount: number;
+    userId: number;
+    userEmail: string;
+    addressId: number;
+    addressLine1: string;
+    addressLine2: string;
+    city: string;
+    state: string;
+    zipCode: string;
+    country: string;
+    paymentMethod: string;
+    orderItems: OrderItem[];
+    createdAt: string;
+  };
+  
+  export type OrderResponseWrapper = {
+    timestamp: string;
+    status: number;
+    message: string;
+    data: Order;
+  };
+  
+  export type OrdersResponseWrapper = {
+    timestamp: string;
+    status: number;
+    message: string;
+    data: Order[];
+  };
